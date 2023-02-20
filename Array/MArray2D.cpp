7a4d44590c89061f3a52f9d8d07358a9ff489feb
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-02-20 20:01:21 +0530

    Added MArray2D class

---
+++ Array/MArray2D.cpp
@@ -0,0 +1,37 @@
+﻿#include "MArray2D.h"
+
+
+MArray2D::MArray2D(int row , int column): Row(row) , Column(column)
+
+{
+    generate_elements(Row,Column);
+}
+
+int MArray2D::GetSize()
+{
+    return Row*Column;
+}
+
+int MArray2D::GetLength()
+{
+    return sizeof(Items)/sizeof(Items[0][0]);
+}
+
+MArray2D::~MArray2D()
+{
+    for (int i = 0; i < Row; i++)
+    {
+        delete[]Items[i];
+    }
+    delete [] Items;
+    
+}
+
+void MArray2D::generate_elements(int item_number , int element_number )
+{
+    Items = new int*[Row];
+    for (int i = 0; i < item_number; i++)
+    {
+        Items[i] = new int [element_number];
+    } 
+}
---
+++ Array/MArray2D.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+//! THIS IS a class called MArray2D which manages creation and deletion of the  array
+class MArray2D 
+{
+private:
+    //Numebr of row
+    int Row;
+
+    //Numberof column 
+    int Column;
+    
+    //TheAcutal Array storing the values
+    int** Items ;
+    
+    // Number of equipped Slots in the array 
+    int length ;
+
+    //Total number of element can be stored in this array 
+    int Size ;
+    
+public:
+    //constructor
+    MArray2D(int row , int column);
+    
+    //Destructor
+    ~MArray2D();
+    
+    //Generation Element for 
+    void generate_elements (int item_number , int element_number );
+
+    //GETTERS
+    int GetSize();
+    int GetLength()
+
+
+    //SETTERS
+    
+    void SetRow ( int rowindex , int* RowArray);
+    void SetColumn (int columnindex , int* ColumnArray );
+
+    // OPERATIONS
+
+    // Ad a single element to the end of the array
+    void Add();
+    // remove a element from the end of the array
+    void Remove ();
+    //Append another array into this 
+    void Append();
+    //insert  a element ata particular index
+    void Inserrt (int index);
+
+    int*& operator[](int row) 
+    {
+        return Items[row];
+    } 
+};
