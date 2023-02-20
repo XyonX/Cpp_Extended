d4273e38fafb029205541aab3fb3da03b88adde6
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-02-20 21:41:57 +0530

    Added Dynamic Funcanality
    Array can be dynamically enlarged

--- Array/MArray.cpp
+++ Array/MArray.cpp
@@ -1,1 +1,52 @@
 ﻿#include "MArray.h"
+
+
+template <class T>
+void MArray<T>::Add(T element)
+{
+    if(Length ==Size)
+    {
+        DynamicResize();
+    }
+    Item[Length];
+    Length++;
+}
+
+template <class T>
+MArray<T>::MArray()
+{
+    BaseSize =8;
+    ResizeFactor =2;
+    Item = new T [BaseSize];
+    CurrentSize =BaseSize ; 
+}
+
+template <class T>
+MArray<T>::~MArray()
+{
+    
+}
+
+template <class T>
+void MArray<T>::DynamicResize()
+{
+    if(Size ==Length)
+    {
+        CurrentSize*=ResizeFactor ;
+    
+        //Creating a array of  new size
+        T* newItems = new T[CurrentSize];
+
+        // using this instead of loop to copy from one to another array
+        memcpy(newItems, Item, Length * sizeof(T));
+
+        // deletign the old allocated memory block  first assigned 
+        delete [] Item;
+        Item = newItems ;
+        Size=CurrentSize;
+        
+    }
+    
+
+    
+}
--- Array/MArray.h
+++ Array/MArray.h
@@ -1,15 +1,56 @@
 ﻿#pragma once
 
+template<class T>
 class MArray
 {
+
+private:
+
+    //Actual container stores the arrray
+    //pointer pointign to a memory location in heap
+    T* Item ;
+
+    // size indicated all the slot of array 
+    int Size ;
+
+    // Length indicates  number of equipped slots
+    int Length ;
+
+    // the base dynamic  aray size 
+    int BaseSize ;
+
+    //this indiace thesize after dynamically  Resizing the array
+    int CurrentSize ;
+
+    //this indicates the Factor at which the array should grow when need 
+    int ResizeFactor ;
+
+    //this indiaced if the array is filled or not 
+    bool bISFilled ;
+
+    
 public:
-    int* Elements ;
-    int size ;
+
+    //GETTERS
+    inline T* Get() { return Item; }
+    inline int size (){return Size;}
+    inline int length() { return Length; }
+
+    //OPERATIONS
+
+    void Add (T element);
+    
+    
 
     //Conatructor 
     MArray();
+    //MArray( );
+    
     //Destructor
     ~MArray();
-    
+
+    void DynamicResize ( );
     
 };
+
+
